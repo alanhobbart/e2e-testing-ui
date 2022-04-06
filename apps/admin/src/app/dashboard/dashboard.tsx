ff1527d59dd3d2f0caa6d9e19790fe57cc533605
alanhobbart <ahobbart+1@gmail.com>
2022-04-06 13:37:13 +0100

    admin ui change

--- apps/admin/src/app/dashboard/dashboard.tsx
+++ apps/admin/src/app/dashboard/dashboard.tsx
@@ -3,7 +3,7 @@ export interface DashboardProps {}
 export function Dashboard(props: DashboardProps) {
   return (
     <div>
-      <h1>Welcome admin</h1>
+      <h1>Welcome admin change</h1>
     </div>
   );
 }
